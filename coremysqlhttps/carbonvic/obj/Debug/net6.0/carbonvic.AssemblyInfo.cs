22b474a3f244ee4a180e6afb58f73525d7e443ee
GreenH <huangshixin47@gmail.com>
2023-08-04 13:33:30 +1000

    json response

--- coremysqlhttps/carbonvic/obj/Debug/net6.0/carbonvic.AssemblyInfo.cs
+++ coremysqlhttps/carbonvic/obj/Debug/net6.0/carbonvic.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("89c52de2-182d-4d97-bfca-20a972f3a331")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("carbonvic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
