826e691d7a87d9999f63db6fecb7b3a072e9e936
GreenH <huangshixin47@gmail.com>
2023-08-24 15:15:16 +1000

    dropdown

---
+++ prototype/demoapp/demoapp/Controllers/DiyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demoapp.Controllers
+{
+    public class DiyController : Controller
+    {
+
+        public IActionResult item1()
+        {
+            return View();
+        }
+
+        public IActionResult item2()
+        {
+            return View();
+        }
+
+        public IActionResult item3()
+        {
+            return View();
+        }
+    }
+}
