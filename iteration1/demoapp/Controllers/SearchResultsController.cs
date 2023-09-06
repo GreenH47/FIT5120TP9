42c3c15b4c3f8b579d7b7143a8cfc1dee803262c
GreenH <huangshixin47@gmail.com>
2023-09-06 16:47:45 +1000

    0906

---
+++ iteration1/demoapp/Controllers/SearchResultsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace demoapp.Controllers
+{
+    public class SearchResultsController : Controller
+    {
+        public async Task<IActionResult> Index(string search, string range)
+        {
+            var searchObject = new { searchWord = search, searchRange = range };
+            var searchJson = JsonConvert.SerializeObject(searchObject);
+
+            ViewData["SearchJson"] = searchJson;
+            ViewData["SearchWord"] = search;
+            ViewData["SearchRange"] = range;
+
+            var apiUrl = "https://2rfrtk7vz4.execute-api.us-east-1.amazonaws.com/deploy";
+            var content = new StringContent(searchJson, Encoding.UTF8, "application/json");
+
+            
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(apiUrl, content);
+                var searchResultJson = await response.Content.ReadAsStringAsync();
+                ViewData["SearchResult"] = searchResultJson;
+
+            }
+            
+
+            return View("Index");
+
+        }
+    }
+
+
+
+}
