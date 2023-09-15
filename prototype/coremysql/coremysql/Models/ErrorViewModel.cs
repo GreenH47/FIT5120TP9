ed7382ab8ec5a5fd96e30bbc86f5b5461c895444
Yuntaozhang97 <yzha0870@student.monash.edu>
2023-09-15 15:55:40 +1000

    Web ui

--- prototype/coremysql/coremysql/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace coremysql.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
