42044c5bc0badeb9e1bc7f09a03950bcb6a2f3f5
GreenH <huangshixin47@gmail.com>
2023-10-02 21:29:41 +1100

    favicon

--- iteration3/flask/app.py
+++ iteration3/flask/app.py
@@ -1,42 +1,68 @@
-from flask import Flask, render_template,request, jsonify
+from flask import Flask, render_template, request, Response
 
-from lambda_py.council_lambda import lambda_handler as know_your_area_lambda_handler
+# from lambda_py.council_lambda import lambda_handler as know_your_area_lambda_handler
 
 app = Flask(__name__)
 
 '''
 page routes
 '''
+
+
+# Set up basic authentication
+@app.before_request
+def require_basic_auth():
+    auth = request.authorization
+
+    # Verify username and password
+    username = 'guest'
+    password = 'MRzYpG7yonPO8'
+
+    if not auth or auth.username != username or auth.password != password:
+        return Response(
+            'Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'}
+        )
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/index.html')
 def index2():
     return render_template('index.html')
 
+
 @app.route('/glass-bottle.html')
 def glass_bottle():
     return render_template('glass-bottle.html')
+
+
 @app.route('/find-your-recycler.html')
 def find_your_recycler():
     return render_template('find-your-recycler.html')
+
 
 @app.route('/know-your-area.html')
 def know_your_area():
     return render_template('know-your-area.html')
 
+
 @app.route('/lamp.html')
 def lamp():
     return render_template('lamp.html')
+
 
 @app.route('/lantern.html')
 def lantern():
     return render_template('lantern.html')
 
+
 @app.route('/plastic-bottle.html')
 def plastic_bottle():
     return render_template('plastic-bottle.html')
+
 
 @app.route('/plastic-bottle-coin-storage.html')
 def plastic_bottle_coin_storage():
@@ -53,11 +79,12 @@ def resources():
     return render_template('resources.html')
 
 
-
 '''
 api routes
 '''
 
+
+# too slow then I gave up on run it on flask
 @app.route('/api/rest/know-your-area', methods=['POST'])
 def know_your_area_api():
     pass
@@ -68,7 +95,5 @@ def know_your_area_api():
     # return response  # Return the response as JSON
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
